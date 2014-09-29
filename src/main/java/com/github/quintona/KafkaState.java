7d9832a4351f8e0cd5f62310e623bc2a3c246d4c
yairwein <yairwein@gmail.com>
2014-09-29 16:18:55 +0300

    Add support for getting topic and key from tuple
    breaking old interface!

--- src/main/java/com/github/quintona/KafkaState.java
+++ src/main/java/com/github/quintona/KafkaState.java
@@ -1,76 +1,80 @@
 package com.github.quintona;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import kafka.producer.KeyedMessage;
 import kafka.javaapi.producer.Producer;
-import kafka.javaapi.producer.ProducerData;
 import kafka.producer.ProducerConfig;
-
-import backtype.storm.task.IMetricsContext;
 import storm.trident.state.State;
 import storm.trident.state.StateFactory;
+import backtype.storm.task.IMetricsContext;
 
-public class KafkaState<T> implements State {
+public class KafkaState implements State {
 	
-	ConcurrentLinkedQueue<String> messages = new ConcurrentLinkedQueue<String>();
+	ConcurrentLinkedQueue<KeyedMessage<String, String>> messages = new ConcurrentLinkedQueue<KeyedMessage<String, String>>();
 
 	public static class Options implements Serializable {
-		public String zookeeperHost = "127.0.0.1";
-		public int zookeeperPort = 2181;
+		
+		private static final long serialVersionUID = 1L;
+		public String zookeeperConnectString = "127.0.0.1:2181";
 		public String serializerClass = "kafka.serializer.StringEncoder";
+		public String brokerList = "127.0.0.1:9092";
 		
 		public Options(){}
 
-		public Options(String zookeeperHost, int zookeeperPort, String serializerClass, String topicName) {
-			this.zookeeperHost = zookeeperHost;
-			this.zookeeperPort = zookeeperPort;
+		public Options(String zookeeperConnectString, String serializerClass, String brokerList) {
+			this.zookeeperConnectString = zookeeperConnectString;
 			this.serializerClass = serializerClass;
+			this.brokerList = brokerList;
 		}
 	}
 	
-	public static StateFactory transactional(String topic, Options options) {
-        return new Factory(topic, options, true);
+	public static StateFactory transactional(Options options) {
+        return new Factory(options, true);
     }
 	
 	public static StateFactory nonTransactional(String topic, Options options) {
-        return new Factory(topic, options, false);
+        return new Factory(options, false);
     }
 
 	protected static class Factory implements StateFactory {
 		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 		private Options options;
-		private String topic;
 		boolean transactional;
 		
-		public Factory(String topic, Options options, boolean transactional){
+		public Factory(Options options, boolean transactional){
 			this.options = options;
-			this.topic = topic;
 			this.transactional = transactional;
 		}
 
 		@Override
 		public State makeState(Map conf, IMetricsContext metrics,
 				int partitionIndex, int numPartitions) {
-			return new KafkaState(topic, options, transactional);
+			return new KafkaState(options, transactional);
 		}
 
 	}
 	
 	private Options options;
-	private String topic;
 	Producer<String, String> producer;
 	private boolean transactional;
 	
-	public KafkaState(String topic, Options options, boolean transactional){
-		this.topic = topic;
+	public KafkaState(Options options, boolean transactional){
 		this.options = options;
 		this.transactional = transactional;
 		Properties props = new Properties();
-		props.put("zk.connect", options.zookeeperHost + ":" + Integer.toString(options.zookeeperPort));
+		props.put("zk.connect", options.zookeeperConnectString);
 		props.put("serializer.class", options.serializerClass);
+		props.put("metadata.broker.list", options.brokerList);
 		ProducerConfig config = new ProducerConfig(props);
 		producer = new Producer<String, String>(config);
 	}
@@ -81,25 +85,30 @@ public class KafkaState<T> implements State {
 			throw new RuntimeException("Kafka State is invalid, the previous transaction didn't flush");
 	}
 	
-	public void enqueue(String message){
+	public void enqueue(KeyedMessage<String, String> message){
 		if(transactional)
 			messages.add(message);
 		else
 			sendMessage(message);
 	}
 	
-	private void sendMessage(String message){
-		ProducerData<String, String> data = new ProducerData<String, String>(topic, message);
-		producer.send(data);
+	private void sendMessage(KeyedMessage<String, String> message){
+		producer.send(message);
+	}
+	
+	private void sendBatch(List<KeyedMessage<String, String>> messages){
+		producer.send(messages);
 	}
 
 	@Override
 	public void commit(Long txid) {
-		String message = messages.poll();
+		KeyedMessage<String, String> message = messages.poll();
+		List<KeyedMessage<String, String>> toSend = new ArrayList<KeyedMessage<String,String>>();
 		while(message != null){
-			sendMessage(message);
+			toSend.add(message);
 			message = messages.poll();
 		}
+		sendBatch(toSend);
 	}
 
 }
--- src/main/java/com/github/quintona/KafkaStateUpdater.java
+++ src/main/java/com/github/quintona/KafkaStateUpdater.java
@@ -2,6 +2,7 @@ package com.github.quintona;
 
 import java.util.List;
 
+import kafka.producer.KeyedMessage;
 import storm.trident.operation.TridentCollector;
 import storm.trident.state.BaseStateUpdater;
 import storm.trident.tuple.TridentTuple;
@@ -10,9 +11,13 @@ public class KafkaStateUpdater extends BaseStateUpdater<KafkaState> {
 
 	private static final long serialVersionUID = -3657719900919830955L;
 	private String messageFieldName;
+	private String topicFieldName;
+	private String keyFieldName;
 
-	public KafkaStateUpdater(String messageFieldName) {
-		this.messageFieldName = messageFieldName;
+	public KafkaStateUpdater(String messageFieldName, String topicFieldName, String keyFieldName) {
+		this.topicFieldName = topicFieldName;
+		this.keyFieldName = keyFieldName;
+		this.messageFieldName = messageFieldName;		
 	}
 
 	@Override
@@ -20,8 +25,13 @@ public class KafkaStateUpdater extends BaseStateUpdater<KafkaState> {
 			TridentCollector collector) {
 		for (TridentTuple t : tuples) {
 			try{
-				if(t.size() > 0)
-					state.enqueue(t.getStringByField(messageFieldName));
+				if(t.size() > 0) {
+					String topic = (String) t.getStringByField(topicFieldName);
+					String key = (String) t.getStringByField(keyFieldName);
+					String message = t.getStringByField(messageFieldName);
+					
+					state.enqueue(new KeyedMessage<String, String>(topic, key, message));
+				}
 			}catch(Exception e){
 				e.printStackTrace();
 			}
